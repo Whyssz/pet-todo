d3bc4d934c59f04cf78a3240a47b23850c79faa3
Shilov Dmitry <dragoniiix@mail.ru>
2022-07-21 00:05:00 +0300

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import * as flsFunctions from './modules/functions.js';
+
+flsFunctions.isWeb();
+console.log('test');
+// import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+
+// const swiper = new Swiper('.swiper', {
+//   loop: true,
+//   slidesPerView: 'auto',
+//   spaceBetween: 100,
+//   modules: [Navigation, Pagination, Autoplay],
+//   autoplay: {
+//     delay: 3000,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+
