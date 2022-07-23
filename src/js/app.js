8a601bea6610e7ae5890b1187a3d2265cf9c4ff6
Shilov Dmitry <dragoniiix@mail.ru>
2022-07-23 17:27:35 +0300

    add makeup

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,11 @@
-import * as flsFunctions from './modules/functions.js';
 
-flsFunctions.isWeb();
-console.log('test');
-// import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+window.addEventListener('DOMContentLoaded',() => {
+  'use strict';
 
-// const swiper = new Swiper('.swiper', {
-//   loop: true,
-//   slidesPerView: 'auto',
-//   spaceBetween: 100,
-//   modules: [Navigation, Pagination, Autoplay],
-//   autoplay: {
-//     delay: 3000,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+
+  
+  console.log('test is success');
+  // flsFunctions.isWeb();
+});
 
 
